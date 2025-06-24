4dffce18444787438b9c1e7fd5c3016e0cce153b
Senitha-EminentB <203494039+Senitha-EminentB@users.noreply.github.com>
2025-06-24 13:53:36 +0530

    Update App.js

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,7 @@ const errorHandler = require('./middlewares/error');
 
 // Route files
 const authRoutes = require('./routes/authRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes');
 
 // Create express app
 const app = express();
@@ -22,6 +23,7 @@ app.use(morgan('dev'));
 
 // Mount routers
 app.use('/api/auth', authRoutes);
+app.use('/api/dashboard', dashboardRoutes);
 
 // Error handler middleware (should be last)
 app.use(errorHandler);
