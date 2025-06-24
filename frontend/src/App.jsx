acfb239f41f70d3429094da526c2c408734a5ad2
Senitha-EminentB <203494039+Senitha-EminentB@users.noreply.github.com>
2025-06-24 16:39:10 +0530

    implementation of frontend
    Instaling React Vite and Material UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <Button variant="contained" color="primary">
+        Hello MUI
+      </Button>
+    </div>
+  );
+}
+
+export default App;
